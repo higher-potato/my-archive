f8037893be8f7b498fd073232e1778fcabda6f04
Jina <jina@Jina.local>
2026-04-27 23:20:00 +0900

    사이트 전체 디자인 개편

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Archive';
+export const SITE_DESCRIPTION = '보고 듣고 읽은 것들의 기록';
