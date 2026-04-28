924e922f003d60592ec86e6b9e365ab614197f2c
Jina <jina@Jina.local>
2026-04-28 15:46:55 +0900

    카테고리 태그 표시

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,6 +14,9 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
+			category: z.string().optional(),
+			author: z.string().optional(),
+			rating: z.number().optional(),
 		}),
 });
 
